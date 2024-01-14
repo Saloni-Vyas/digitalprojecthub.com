d80d5beced81f688f1b2182fff7de558851437c1
Rohit Bhure <rohitbhute.cse@gmail.com>
2024-01-14 12:08:41 +0530

    now replace pid=slug

--- include/testinomial.php
+++ include/testinomial.php
@@ -12,15 +12,15 @@ color: linear-gradient(344deg, rgba(161,54,130,1) 0%, rgba(88,48,179,1) 61%);">P
         <div class="mx-auto max-w-4xl mt-10">
           <div class="md:flex md:items-center md:justify-center md:space-x-14">
             <div class="relative h-48 w-48 flex-shrink-0">
-              <img class="relative h-48 w-48 shadow-2xl rounded-full object-cover" src="<?php echo weburl;?>/images/team/divyansh.jpg" decoding="async" loading="lazy" alt="" />
+              <img class="relative h-48 w-48 shadow-2xl rounded-full object-cover" src="<?php echo weburl; ?>/images/user/priyansh.jpg" decoding="async" loading="lazy" alt="" />
             </div>
             <div class="mt-10 md:mt-0">
               <blockquote>
                 <p class="text-xl text-black">
-                  “<?php echo webname;?> has truly been a game-changer for my academic journey. The variety and quality of projects available on the website surpassed my expectations. The user-friendly interface made it easy for me to find and purchase the perfect project for my computer science course.”
+                  “<?php echo webname; ?> has truly been a game-changer for my academic journey. The variety and quality of projects available on the website surpassed my expectations. The user-friendly interface made it easy for me to find and purchase the perfect project for my computer science course.”
                 </p>
               </blockquote>
-              <p class="mt-7 text-lg font-semibold text-black">Rahul Verma</p>
+              <p class="mt-7 text-lg font-semibold text-black">Priyansh Malakar</p>
               <p class="mt-1 text-base text-gray-600">Pursuing BTech</p>
             </div>
           </div>
